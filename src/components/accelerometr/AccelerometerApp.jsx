6229db42653cad757a74d46437929016f9063c70
unknown <pusurmankulovdami10@gmail.com>
2024-05-03 15:48:07 +0600

    four commit

--- src/components/accelerometr/AccelerometerApp.jsx
+++ src/components/accelerometr/AccelerometerApp.jsx
@@ -2,20 +2,23 @@ import { useEffect, useState } from "react";
 import "./AccelerometerApp.css";
 
 const AccelerometerApp = () => {
-  const [acceleration, setAcceleration] = useState({ x: 0, y: 0, z: 0 });
+  const [speed, setSpeed] = useState(0);
   const [mode, setMode] = useState("Неактивный"); 
 
   useEffect(() => {
     const handleAcceleration = (event) => {
       const { x, y, z } = event.acceleration;
-      setAcceleration({ x, y, z });
+      const acceleration = Math.sqrt(x * x + y * y + z * z);
+      // Преобразование ускорения в скорость в км/ч (примерное)
+      const calculatedSpeed = acceleration * 3.6; // Перевод из м/c в км/ч
 
-      const speed = Math.sqrt(x * x + y * y + z * z);
-      if (speed < 5) {
+      setSpeed(calculatedSpeed);
+
+      if (calculatedSpeed < 5) {
         setMode("Неактивный");
-      } else if (speed >= 5 && speed < 15) {
+      } else if (calculatedSpeed >= 5 && calculatedSpeed < 15) {
         setMode("Пешком ");
-      } else if (speed >= 15) {
+      } else if (calculatedSpeed >= 15) {
         setMode("Активный");
       }
     };
@@ -29,12 +32,8 @@ const AccelerometerApp = () => {
 
   return (
     <div className={`app-container ${mode}`}>
-      <h1>Статус: {mode}</h1>
-      <div className="acceleration-data">
-        <p>Ускорение X: {acceleration.x}</p>
-        <p>Ускорение Y: {acceleration.y}</p>
-        <p>Ускорение Z: {acceleration.z}</p>
-      </div>
+      <h1>Скорость: {speed.toFixed(2)} км/ч</h1>
+
     </div>
   );
 };
